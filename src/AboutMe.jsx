0f16f24cbc87e1a843474a8aa1cb1614f2dbfeb6
Maimuna Rahman Mysha <maimunarahman30@gmail.com>
2025-03-10 03:02:58 +0600

    Update AboutMe.jsx

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
 const AboutMe = () => {
     return (
-        <div>
-            
+        <div id="about">
+            <h1 className="text-center font-bold text-4xl">About Me</h1>
+            <div className="flex justify-between items-center">
+            <div >
+                <p className="p-5 font-semibold">Hi, I’m Maimuna Rahma, a MERN stack developer with a passion for building dynamic, user-friendly applications. My journey into programming started a few years ago, and since then, I’ve honed my skills in MongoDB, Express.js, React, and Node.js. I love creating seamless user experiences and solving problems with code, whether it’s developing a tech product platform like Gadget Heaven or building a movie portal like Orchid.</p>
+                <p className="p-5 font-semibold ">I’m particularly drawn to React for its flexibility in creating interactive, efficient web applications. Outside of coding, I enjoy nature walks, reading books on self-improvement and technology, and exploring new ideas. I believe in continuous growth, both professionally and personally, and I’m always looking for ways to improve and innovate.</p>
+                <p>I value collaboration and enjoy working with others to bring ideas to life. Feel free to explore my GitHub to see some of my work, and let’s connect!</p>
+            </div>
+            </div>
         </div>
     );
 };
